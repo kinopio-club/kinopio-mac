45f611eb8ecbe45602702016ee6d194d535ab2e1
Pirijan <hi@pirijan.com>
2020-08-11 11:46:16 -0400

    Initial Commit

---
+++ Kinopio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kinopio
+//
+//  Created by pirijan on 8/11/20.
+//  Copyright © 2020 Kinopio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Kinopio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kinopio
+//
+//  Created by pirijan on 8/11/20.
+//  Copyright © 2020 Kinopio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
